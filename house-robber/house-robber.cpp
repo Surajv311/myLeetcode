099192574b9a5417db4f7fcb57e0774f06340a3c
Suraj_v <59371846+Surajv311@users.noreply.github.com>
2021-07-14 14:18:53 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (94.21%) - LeetHub

---
+++ house-robber/house-robber.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int dp[10001]; 
+    
+    int fun(vector<int> &nums,int n,int &ans){
+        
+        if(n<0) return 0;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        int a =  nums[n] + fun(nums,n-2,ans);
+        int b = fun(nums,n-1,ans); 
+        
+        ans = max(a,b);
+        dp[n] = ans; 
+        
+        return dp[n];
+    }
+    int rob(vector<int>& nums) {
+        
+        if(nums.size()==1) return nums[0];
+       
+        memset(dp,-1,sizeof(dp));
+        int n = nums.size();
+        int ans = 0;
+        fun(nums,n-1,ans);
+      
+        return ans;  
+        }
+};
