a78e4574bacf0241e6facb128ead6fad75e68b5c
Suraj_v <59371846+Surajv311@users.noreply.github.com>
2021-09-03 00:30:15 +0530

    Time: 3 ms (74.33%), Space: 13.7 MB (49.48%) - LeetHub

---
+++ generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    void fun(int open, int close, vector<string>& ans, string s){
+        
+        if(open==0 and close==0){
+            ans.push_back(s); 
+            return; 
+        }
+        
+        if(open!=0){
+            // string a = "("; 
+            // s.append("(");
+            // s+=a; 
+            fun(open-1,close,ans,s+"(" ); 
+        }
+        
+        // using if & not else if... 
+        
+        if(open<close){
+            // string a = ")"; 
+            // s+=a; 
+            fun(open,close-1,ans,s+")" ); 
+        }
+        return; 
+    }
+    
+    vector<string> generateParenthesis(int n) {
+     int open, close; 
+        open = close = n; 
+        vector<string> ans; 
+        fun(open,close, ans,"");   
+        
+        return ans; 
+    }
+};
