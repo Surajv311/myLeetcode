a60f189f6a6b4559d7750d345c13e4cc59bddc1c
Suraj_v <59371846+Surajv311@users.noreply.github.com>
2021-09-04 23:30:26 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (96.91%) - LeetHub

--- combination-sum/combination-sum.cpp
+++ combination-sum/combination-sum.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     
-void recur(vector<int>& arr, int target, vector<int> &ds ,  vector<vector<int>>& ans,int i){
+void recur(vector<int>& arr, int target, vector<int> &ds ,  vector<vector<int>>& ans,int ind){
     
+// note that numbers can be chosen unlimited number of times.     
 //     base case
     
-    if(i==arr.size()) {   
+    if(ind==arr.size()) {   
+//         ind reaches arr.size(), when during further calls, when arr[ind]> target...our ind+1...
       if(target==0) ans.push_back(ds);
         return;
     }
     
     
 //     including the number
-//     when including , ensure its lesss than target
+//     when including , ensure it is less than target
     
-    if(arr[i]<=target){
-        ds.push_back(arr[i]);
-        recur(arr,target-arr[i],ds,ans,i); // hence 'i' is same...
+    if(arr[ind]<=target){
+        ds.push_back(arr[ind]);
+        recur(arr,target-arr[ind],ds,ans,ind); // hence 'i' is same...
         ds.pop_back();
     }
     
 //         skipping the number
     
-    recur(arr,target,ds,ans,i+1);
+    recur(arr,target,ds,ans,ind+1);
         
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
-        
+        sort(candidates.begin(), candidates.end());
         
         vector<vector<int>> ans; 
         vector<int> ds; 
-        int i = 0 ; 
-        recur(candidates,target,ds,ans,i);
+        int ind = 0 ; 
+        recur(candidates,target,ds,ans,ind);
         
         return ans;
         
