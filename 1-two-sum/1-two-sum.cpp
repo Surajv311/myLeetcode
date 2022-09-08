5132f942041b61e4da7830eb28975a55a40ece42
Suraj Verma <59371846+Surajv311@users.noreply.github.com>
2022-09-08 20:38:57 +0530

    Time: 16 ms (77.46%), Space: 12.1 MB (8.71%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -21,5 +21,30 @@ public:
         return ans; 
         
         
+        /*
+        
+        for case of not using a map ~ using sorting + 2 pointer ~ o(nlogn)
+        
+         int n = nums.size();
+        vector<pair<int, int>> arr(n);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = make_pair(nums[i], i);
+        }
+        sort(arr.begin(), arr.end()); // Sort arr in increasing order by their values.
+        int left = 0, right = n- 1;
+        while (left < right) {
+            int sum2 = arr[left].first + arr[right].first;
+            if (sum2 == target) 
+                return {arr[left].second, arr[right].second};
+            if (sum2 > target)
+                right -= 1; // Try to decrease sum2
+            else
+                left += 1; // Try to increase sum2
+        }
+        return {};
+        
+        
+        */
+        
     }
 };
