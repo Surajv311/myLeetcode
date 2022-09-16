2cbc3434a30027f8ef1d0977301458e6a7188da3
Suraj Verma <59371846+Surajv311@users.noreply.github.com>
2022-09-16 11:13:46 +0530

    Time: 179 ms (75.52%), Space: 83.9 MB (30.23%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        
+        
+        for(int i = 0 ; i < nums.size();i++) 
+        {
+            if(nums[i]==0) nums[i]=-1; 
+        }
+        
+        
+        int r = 0 ; 
+        int sum = 0 ;
+        unordered_map<int,int> mp; 
+        
+        mp[0] = -1; // initialised for 0 sum 
+        
+        for(int i = 0; i< nums.size(); i++){
+            
+            sum+=nums[i]; 
+            
+            // if(sum==0) r = max(r,i);
+            
+            if(mp.find(sum)!=mp.end()){
+                r = max(r,i-mp[sum]);
+            } 
+            
+            else mp[sum] = i; 
+            
+        }
+        
+        return r; 
+        
+    }
+};
