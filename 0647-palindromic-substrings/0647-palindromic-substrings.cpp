38bb9091605ecc514539b2a4d53f0277e2ff5bc6
Suraj Verma <59371846+Surajv311@users.noreply.github.com>
2022-10-17 11:34:10 +0530

    Time: 432 ms (12.82%), Space: 299.9 MB (10.92%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -25,4 +25,35 @@ public:
         
         return vs.size();
     }
+    
+    /* 
+    
+    using dp 
+    
+       string s;
+    vector<vector<int>>dp;
+    
+    int f(int l , int r){
+        if(l>r)return 1;
+        if(l==r)return 1;
+        if(dp[l][r]!=-1)return dp[l][r];
+        if(s[l]!=s[r])return dp[l][r]=0;
+        if(f(l+1,r-1))return dp[l][r]=1;
+        return dp[l][r]=0;
+    }
+    
+    int countSubstrings(string s) {
+        this->s=s;
+        int ans=0,n=s.size();
+        dp.resize(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                ans+=f(i,j);
+            }
+        }
+        return ans;
+    }
+    
+    */
+    
 };
